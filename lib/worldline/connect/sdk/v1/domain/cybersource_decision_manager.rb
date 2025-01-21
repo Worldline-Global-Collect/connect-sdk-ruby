297e582e9664e825f2f4e8e6d2a0dc79d427cd0c
WorldlineConnect <github.connect@worldline.com>
2025-01-21 13:54:18 +0100

    Release 3.3.0.

--- lib/worldline/connect/sdk/v1/domain/cybersource_decision_manager.rb
+++ lib/worldline/connect/sdk/v1/domain/cybersource_decision_manager.rb
@@ -10,20 +10,12 @@ module Worldline
       module V1
         module Domain
           # @attr [String] clause_name
-          # @attr [String] device_country_code
-          # @attr [String] device_id
           # @attr [Integer] fraud_score
           # @attr [String] policy_applied
           # @attr [Array<String>] reason_codes
-          # @attr [String] true_ip_address
-          # @attr [String] user_device_type
-          class MicrosoftFraudResults < Worldline::Connect::SDK::Domain::DataObject
+          class CybersourceDecisionManager < Worldline::Connect::SDK::Domain::DataObject
 
             attr_accessor :clause_name
-
-            attr_accessor :device_country_code
-
-            attr_accessor :device_id
 
             attr_accessor :fraud_score
 
@@ -31,21 +23,13 @@ module Worldline
 
             attr_accessor :reason_codes
 
-            attr_accessor :true_ip_address
-
-            attr_accessor :user_device_type
-
             # @return (Hash)
             def to_h
               hash = super
               hash['clauseName'] = @clause_name unless @clause_name.nil?
-              hash['deviceCountryCode'] = @device_country_code unless @device_country_code.nil?
-              hash['deviceId'] = @device_id unless @device_id.nil?
               hash['fraudScore'] = @fraud_score unless @fraud_score.nil?
               hash['policyApplied'] = @policy_applied unless @policy_applied.nil?
               hash['reasonCodes'] = @reason_codes unless @reason_codes.nil?
-              hash['trueIpAddress'] = @true_ip_address unless @true_ip_address.nil?
-              hash['userDeviceType'] = @user_device_type unless @user_device_type.nil?
               hash
             end
 
@@ -53,12 +37,6 @@ module Worldline
               super
               if hash.has_key? 'clauseName'
                 @clause_name = hash['clauseName']
-              end
-              if hash.has_key? 'deviceCountryCode'
-                @device_country_code = hash['deviceCountryCode']
-              end
-              if hash.has_key? 'deviceId'
-                @device_id = hash['deviceId']
               end
               if hash.has_key? 'fraudScore'
                 @fraud_score = hash['fraudScore']
@@ -72,12 +50,6 @@ module Worldline
                 hash['reasonCodes'].each do |e|
                   @reason_codes << e
                 end
-              end
-              if hash.has_key? 'trueIpAddress'
-                @true_ip_address = hash['trueIpAddress']
-              end
-              if hash.has_key? 'userDeviceType'
-                @user_device_type = hash['userDeviceType']
               end
             end
           end
--- lib/worldline/connect/sdk/v1/domain/fraud_results.rb
+++ lib/worldline/connect/sdk/v1/domain/fraud_results.rb
@@ -3,6 +3,7 @@
 # https://apireference.connect.worldline-solutions.com/
 #
 require 'worldline/connect/sdk/domain/data_object'
+require 'worldline/connect/sdk/v1/domain/cybersource_decision_manager'
 require 'worldline/connect/sdk/v1/domain/in_auth'
 require 'worldline/connect/sdk/v1/domain/microsoft_fraud_results'
 
@@ -11,10 +12,13 @@ module Worldline
     module SDK
       module V1
         module Domain
+          # @attr [Worldline::Connect::SDK::V1::Domain::CybersourceDecisionManager] cybersource_decision_manager
           # @attr [String] fraud_service_result
           # @attr [Worldline::Connect::SDK::V1::Domain::InAuth] in_auth
           # @attr [Worldline::Connect::SDK::V1::Domain::MicrosoftFraudResults] microsoft_fraud_protection
           class FraudResults < Worldline::Connect::SDK::Domain::DataObject
+
+            attr_accessor :cybersource_decision_manager
 
             attr_accessor :fraud_service_result
 
@@ -25,6 +29,7 @@ module Worldline
             # @return (Hash)
             def to_h
               hash = super
+              hash['cybersourceDecisionManager'] = @cybersource_decision_manager.to_h unless @cybersource_decision_manager.nil?
               hash['fraudServiceResult'] = @fraud_service_result unless @fraud_service_result.nil?
               hash['inAuth'] = @in_auth.to_h unless @in_auth.nil?
               hash['microsoftFraudProtection'] = @microsoft_fraud_protection.to_h unless @microsoft_fraud_protection.nil?
@@ -33,6 +38,10 @@ module Worldline
 
             def from_hash(hash)
               super
+              if hash.has_key? 'cybersourceDecisionManager'
+                raise TypeError, "value '%s' is not a Hash" % [hash['cybersourceDecisionManager']] unless hash['cybersourceDecisionManager'].is_a? Hash
+                @cybersource_decision_manager = Worldline::Connect::SDK::V1::Domain::CybersourceDecisionManager.new_from_hash(hash['cybersourceDecisionManager'])
+              end
               if hash.has_key? 'fraudServiceResult'
                 @fraud_service_result = hash['fraudServiceResult']
               end
